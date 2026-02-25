06ec2ad1905ae862ca74b97363c3658dab7eb7bd
RussiaTails <113246593+RussiaTails@users.noreply.github.com>
2026-02-25 15:46:21 +0300

    Gray Gravel map support

--- utils/RCBot2_meta/bot_tf2_points.cpp
+++ utils/RCBot2_meta/bot_tf2_points.cpp
@@ -786,9 +786,19 @@ bool CTFObjectiveResource :: updateAttackPoints (const int team)
 	std::memset(arr,0,sizeof(TF2PointProb_t)*MAX_CONTROL_POINTS);
 	std::memset(arr->iPrev,0xFF,sizeof(int)*MAX_PREVIOUS_POINTS);
 
-	if ( team == TF2_TEAM_RED && CTeamFortress2Mod::isAttackDefendMap() )
+	if ( team == TF2_TEAM_RED && (CTeamFortress2Mod::isAttackDefendMap() && !std::strncmp(szmapname, "cp_graygravel_b2", 16) == 0))
 	{
+		if (team == TF2_TEAM_RED && std::strncmp(szmapname, "cp_graygravel_b2", 16) == 0)
+		{
+			return true;
+		}
 		// no attacking for red on this map
+		return false;
+	}
+
+	if (team == TF2_TEAM_BLUE && std::strncmp(szmapname, "cp_graygravel_b2", 16) == 0)
+	{
+		// no attacking for blu on this map
 		return false;
 	}
 
